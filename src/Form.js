7ba9ff0516d30da7de11b8d20d83d9f0f92e79a3
lgfh98 <49377574+lgfh98@users.noreply.github.com>
2021-09-03 10:52:53 -0500

    manejo del estado

--- src/Form.js
+++ src/Form.js
@@ -2,13 +2,26 @@ import React from "react";
 
 export default function Form(props) {
   const { movie } = props;
+  const [quantity, setQuantity] = React.useState(0);
 
   return (
     <form>
       <h3>{movie.name}</h3>
-      <button>+</button>
-      <span>0</span>
-      <button>-</button>
+      <button
+        type="button"
+        onClick={() => setQuantity(quantity - 1)}
+        disabled={quantity <= 0}
+      >
+        -
+      </button>
+      <span>{quantity}</span>
+      <button
+        type="button"
+        onClick={() => setQuantity(quantity + 1)}
+        disabled={quantity === movie.availables}
+      >
+        +
+      </button>
     </form>
   );
 }
